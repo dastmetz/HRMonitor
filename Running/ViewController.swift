2028560d4a07a7e5ac9bfd050eb4fc9c61f6109e
Daniel Steinmetz <daniel.steinmetz@posteo.de>
2016-03-21 21:25:42 +0100

    Initial Commit

---
+++ Running/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running
+//
+//  Created by Daniel Steinmetz on 21.03.16.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
